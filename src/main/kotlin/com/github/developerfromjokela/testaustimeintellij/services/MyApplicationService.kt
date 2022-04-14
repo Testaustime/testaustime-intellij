745fab7b3251233c2a8c7f1ce79decb8306a7b02
GitHub Action <action@github.com>
2022-04-14 07:51:56 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/developerfromjokela/testaustimeintellij/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.developerfromjokela.testaustimeintellij.services
+
+import com.github.developerfromjokela.testaustimeintellij.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/developerfromjokela/testaustimeintellij/services/MyProjectService.kt
+++ src/main/kotlin/com/github/developerfromjokela/testaustimeintellij/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.developerfromjokela.testaustimeintellij.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.developerfromjokela.testaustimeintellij.MyBundle
 
 class MyProjectService(project: Project) {
 
