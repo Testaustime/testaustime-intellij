a77b6375ba284806bb8236805122ad7d0220fe42
bluelhf <ilari.suhonen@gmail.com>
2023-04-14 21:17:38 +0300

    fix(buildscript): actually remove until-build

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,6 +35,8 @@ intellij {
     version.set(properties("platformVersion"))
     type.set(properties("platformType"))
 
+    updateSinceUntilBuild.set(false)
+
     // Plugin Dependencies. Uses `platformPlugins` property from the gradle.properties file.
     plugins.set(properties("platformPlugins").split(',').map(String::trim).filter(String::isNotEmpty))
 }
